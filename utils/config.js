c023d534dce72528ce475f03d089ab63247e3702
宝昌 <32792217+BaoChn@users.noreply.github.com>
2021-12-02 22:14:33 +0800

    Add files via upload

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+var DOMAIN = "a-b.cc";
+var WEBSITENAME="宝昌"; //网站名称
+var PAGECOUNT='10'; //每页文章数目
+export default {
+  getDomain: DOMAIN,
+  getWebsiteName: WEBSITENAME,  
+  getPageCount: PAGECOUNT
+ 
+}
